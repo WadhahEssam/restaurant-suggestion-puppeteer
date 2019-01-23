4765642585ec3034e82357198e799e46baeecc8a
Wadhah Essam Mac <wadah-wleed@hotmail.com>
2019-01-23 11:16:09 +0300

    everything should hopefully be working

--- server.js
+++ server.js
@@ -17,24 +17,22 @@ app.get('/', (req, res) => {
 });
 
 app.post('/getInformation', async (req, res) => {
-  // console.log(req.body);
-  getData(req.body.lat, req.body.long);
-  res.send(`lat : ${req.body.lat} / long : ${req.body.long}`)
+  let data = await getData(req.body.lat, req.body.long);
+  res.send(data);
 })
 
 getData = async (lat, long) => {
   console.log(`lat : ${lat} / long ${long}`);
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
   await page.setViewport({height: 1080, width: 1920});
   await page.goto(`https://wainnakel.com/api/v1/GenerateFS.php?uid=${lat},${long}&get_param=value`);
   let result = await page.evaluate(() => {
     return document.querySelector('body > pre').textContent
   })
-  console.log(result);
-  // document.querySelector('body > pre').textContent
-
-  // await browser.close();
+  console.log(JSON.parse(result));
+  await browser.close();
+  return result;
 }
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
